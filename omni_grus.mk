3d0c2e38eaca89bd07942e413ed8913783631692
Redis <redis@rcs.al>
2019-07-13 16:40:21 +0000

    Initial commit twrp-android-9.0

--- omni_grus.mk
+++ omni_grus.mk
@@ -20,7 +20,7 @@ PRODUCT_RELEASE_NAME := grus
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := grus
