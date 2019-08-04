090d86e922b23357b51ca10d71ca16f2e5738825
redispade <34771407+redispade@users.noreply.github.com>
2019-08-04 17:49:25 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,8 @@
+# apply libvintf patches
+workdir=$(pwd)
+cd system/libvintf || return
+git reset -q --hard
+[ "$target" == "grus" ] && patch --merge -sNp1 -i "$workdir"/device/xiaomi/grus/patches/system-libvintf-disable-vbmeta-check.patch
+cd "$workdir" || return
+
 add_lunch_combo omni_grus-eng
