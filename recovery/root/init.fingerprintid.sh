44a4fbdc1d365d7e250c107ce0d3f707b049b255
Redis <redis@rcs.al>
2020-02-28 18:23:30 +0100

    Update fingerprint emulation script

--- recovery/root/init.fingerprintid.sh
+++ recovery/root/init.fingerprintid.sh
@@ -11,8 +11,8 @@
 # for keymaster-3.0+
 #
 # by wzsx150
-# v2.5-20191014
-# adapted for twrp by redispade
+# v3.0-20191029
+#
 
 SYSTEM_TMP=/tools/system_tmp
 VENDOR_TMP=/tools/vendor_tmp
@@ -26,47 +26,48 @@ do
 done
 [ -z "$systempart" ] && setprop "twrp.fingerprintid.prop" "0"
 #[ -z "$systempart" ] && setprop "twrp.fingerprintid.system" "none"
-[ -z "$systempart" ] && exit 1
-
+[ -z "$systempart" ] && {
+  touch /sbin/fingerprint_ready
+  exit 1
+}
 #vendorpart
-vendorpart=`find /dev/block -name vendor | grep "by-name/vendor" -m 1 2>/dev/null`
+for i in $(seq 0 900)
+do
+  vendorpart=`find /dev/block -name vendor | grep "by-name/vendor" -m 1 2>/dev/null`
+  [ -z "$vendorpart" ] || break
+  usleep 1000
+done
+
 
 mkdir -p "$SYSTEM_TMP"
+mkdir -p "$VENDOR_TMP"
 mount -t ext4 -o ro "$systempart" "$SYSTEM_TMP"
+mount -t ext4 -o ro "$vendorpart" "$VENDOR_TMP"
 usleep 100
 
-temp=`cat "$SYSTEM_TMP/system/build.prop" "$SYSTEM_TMP/build.prop" /default.prop 2>/dev/null`
+temp=`cat "$SYSTEM_TMP/system/build.prop" \
+          "$SYSTEM_TMP/build.prop" \
+          "$VENDOR_TMP/build.prop" \
+          /default.prop 2>/dev/null`
+usleep 100
+
+umount "$SYSTEM_TMP" &
+umount "$VENDOR_TMP" &
 
 fingerprint=`echo "$temp" | grep -F "ro.build.fingerprint=" -m 1 | cut -d'=' -f2` && resetprop "ro.build.fingerprint" "$fingerprint" &
 sys_fingerprint=`echo "$temp" | grep -F "ro.system.build.fingerprint=" -m 1 | cut -d'=' -f2` && resetprop "ro.system.build.fingerprint" "$sys_fingerprint" &
 RELEASE=`echo "$temp" | grep -F "ro.build.version.release=" -m 1 | cut -d'=' -f2` && resetprop "ro.build.version.release" "$RELEASE" &
 PATCH=`echo "$temp" | grep -F "ro.build.version.security_patch=" -m 1 | cut -d'=' -f2` && resetprop "ro.build.version.security_patch" "$PATCH" &
 SDK=`echo "$temp" | grep -F "ro.build.version.sdk=" -m 1 | cut -d'=' -f2` && resetprop "ro.build.version.sdk" "$SDK" &
+VENPATCH=`echo "$temp" | grep -F "ro.vendor.build.security_patch=" -m 1 | cut -d'=' -f2` && resetprop "ro.vendor.build.security_patch" "$VENPATCH" &
 
-
-#vendorpart
-if [ ! -z "$vendorpart" ]; then
-
-  setprop "twrp.fingerprintid.vendor" "$vendorpart" &
-
-  mkdir -p "$VENDOR_TMP"
-  mount -t ext4 -o ro "$vendorpart" "$VENDOR_TMP"
-  usleep 100
-
-  VENPATCH=`cat "$SYSTEM_TMP/system/build.prop" "$SYSTEM_TMP/build.prop" "$VENDOR_TMP/build.prop" /default.prop 2>/dev/null | grep -F "ro.vendor.build.security_patch=" -m 1 | cut -d'=' -f2`
-
-  resetprop "ro.vendor.build.security_patch" "$VENPATCH" &
-
-  umount "$VENDOR_TMP" &
-
-fi
 
 setprop "twrp.fingerprintid.prop" "1" &
-setprop "twrp.fingerprintid.system" "$systempart" &
-
-umount "$SYSTEM_TMP"
-
 setprop "twrp.fingerprintid.sys_root" "1" &
+setprop "twrp.fingerprintid.system" "$systempart" &
+setprop "twrp.fingerprintid.vendor" "$vendorpart" &
+
+touch /sbin/fingerprint_ready
 
 sleep 2
 
