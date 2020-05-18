0e2901a06d509a3f39aa364fdec30542d3211a03
Redis <redis@rcs.al>
2020-05-18 13:30:29 +0200

    remove libvintf patch, causes issues with android 10 roms

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,1 @@
-# apply libvintf patches
-workdir=$(pwd)
-cd system/libvintf || return
-git reset -q --hard
-[ "$target" == "grus" ] && patch --merge -sNp1 -i "$workdir"/device/xiaomi/grus/patches/system-libvintf-disable-vbmeta-check.patch
-cd "$workdir" || return
-
 add_lunch_combo omni_grus-eng
